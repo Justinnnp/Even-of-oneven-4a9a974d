aa8ac0fd5903c78c6853e7b6f1f35eafff026c48
Justin Plein <2099449@talnet.nl>
2020-01-15 09:58:18 +0100

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,10 @@
+<?php
+
+$getal1 = readline("Vul een getal in. ");
+
+    if($getal1 % 2 == 0) {
+        echo("dit is een even getal !");
+    } else {
+        echo("dit is een oneven getal !");
+    }
+?>
